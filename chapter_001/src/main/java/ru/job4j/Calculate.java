f0a2b0d5671831d95bdc834b4386475e89ccc525
PavelKorchaginAleksandrovich <pav.intense@gmail.com>
2017-12-12 00:54:39 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j;
-
+/**
+*@class Calculate Задача 1.3.
+*@author Pavel Korchagin.
+*@since 12.12.2017.
+*@version 1.0.
+*/
 public class Calculate{
+	/**
+	*метод вывода на печать.
+	*@param args.
+	*/
 	public static void main (String[] args){
 		System.out.print("Hello world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/packege-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Pavel Korchagin (pav.intense@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
