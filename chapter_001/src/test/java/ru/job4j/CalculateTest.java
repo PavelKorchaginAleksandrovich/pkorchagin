6341326bfe1b6caa51332e1da3e45f259af9b3f1
PavelKorchaginAleksandrovich <pav.intense@gmail.com>
2017-12-13 14:32:15 +0300

    1.4. JUnit [#4122]

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,29 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+* Test.
+*
+* @author Pavel Kortchagin	
+* @version 1.0
+* @since 13.12.2017
+*/
+public class CalculateTest {
+/**
+* Test echo.
+*/ @Test
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Pablo";
+    String expect = "Echo, echo, echo : Pablo"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+ 
+}
