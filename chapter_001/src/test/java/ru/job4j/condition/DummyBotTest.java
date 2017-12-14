e900d15bea023261ff91e80d85d941b4ca47951a
PavelKorchaginAleksandrovich <pav.intense@gmail.com>
2017-12-14 16:57:38 +0300

    3.1. Глупый бот

---
+++ chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test DummyBot.
+ * @author Pavel Kortchagin
+ * @version 1.0
+ * @since 14.12.2017
+ */
+public class DummyBotTest {
+    DummyBot bot = new DummyBot();
+    @Test
+    public void whenGreetBot() {
+
+        assertThat(
+                bot.answer("Привет, Бот."),
+                is("Привет, умник.")
+        );
+    }
+
+    @Test
+    public void whenByuBot() {
+        assertThat(
+                bot.answer("Пока."),
+                is("До скорой встречи.")
+        );
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        assertThat(
+                bot.answer("Сколько будет 2 + 2?"),
+                is("Это ставит меня в тупик. Спросите другой вопрос.")
+        );
+    }
+}
+
