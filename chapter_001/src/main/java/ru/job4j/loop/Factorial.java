fafd5abb737774218497ac6fbe529c7d983d2a2e
PavelKorchaginAleksandrovich <pav.intense@gmail.com>
2017-12-20 23:41:22 +0300

    4.2. Создать программу вычисляющую факториал.

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -1,2 +1,30 @@
+package ru.job4j.loop;
+
+/**
+ *@class Factorial задача 4.2.
+ *@author Pavel Korchagin.
+ *@since 17.12.2017.
+ *@version 1.0.
+ */
+
+public class Factorial {
+
+    /**
+     * Вычислеие факториала положительного числа,
+     * в случае отрицательного входящего параметра
+     * возращаем 0, иначе будет ошибка.
+     * @param n
+     * @return факториал числа n
+     */
+    public int calc(int  n) {
+        int factorial = 1;
+        for (int count = 1; count <= n; count++) {
+            factorial = count * factorial;
+        }
+        return factorial;
+    }
+}
+
+
 
 
