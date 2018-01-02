3f360230c671a2d0e8fb8203d00db7dd81f715e6
PavelKorchaginAleksandrovich <pav.intense@gmail.com>
2018-01-02 21:45:40 +0300

    1. Реализация профессий в коде

---
+++ chapter_002/src/main/java/tracker/Tracker.java
@@ -0,0 +1,30 @@
+package tracker;
+
+public class Tracker {
+    private Item[] items = new Item[100];
+
+    private int position = 0;
+    private String generateId() {
+        //Реализовать метод генерации.
+        return null;
+    }
+
+    public Item add(Item item) {
+
+    }
+    public void replace(String id, Item item) {
+
+    }
+    public void delete(String id) {
+
+    }
+    public Item[] findAll() {
+
+    }
+    public Item[] findByName(String key) {
+
+    }
+    public Item findById(String id) {
+
+    }
+}
