8ceda35c269153c0e5b757b873ff328b7de6736d
PavelKorchaginAleksandrovich <pav.intense@gmail.com>
2018-01-03 19:58:55 +0300

    2. Реализовать класс Tracker

---
+++ chapter_002/src/main/java/ru/job4j/start/Item.java
@@ -0,0 +1,47 @@
+package ru.job4j.start;
+
+public class Item {
+    private String id;
+    private String description;
+    private long create;
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Item(String id, String description, long create, String name) {
+        this.id = id;
+        this.description = description;
+        this.create = create;
+        this.name = name;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getCreate() {
+        return this.create;
+    }
+
+    public void setCreate(long create) {
+        this.create = create;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/start/Tracker.java
@@ -0,0 +1,77 @@
+package ru.job4j.start;
+
+import java.util.*;
+
+
+public class Tracker {
+    private Item[] items = new Item[100];
+
+    private int position = 0;
+    private static final Random RN = new Random();
+    private String generateId() {
+        return String.valueOf(System.currentTimeMillis() + RN.nextInt());
+    }
+
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        this.items[position++] = item;
+        return item;
+    }
+    public void replace(String id, Item item) {
+        int index = findByIdIndex(id);
+        if (index != -1) {
+            this.items[index] = item;
+        }
+    }
+    public void delete(String id) {
+        int index = findByIdIndex(id);
+        System.arraycopy(items, index, items, index + 1, this.position - index - 1);
+    }
+    public Item[] findAll() {
+        Item[] result = new Item[this.position];
+        for (int index = 0; index != this.position; index++) {
+            result[index] = this.items[index];
+        }
+        return result;
+    }
+
+    public Item[] findByName(String key) {
+        //посчитаем сколько совпадений и найдем индексы совпадений
+        int count = 0;
+        int[] indexes = new int[this.items.length];
+        for (int i = 0; i != this.position; i++) {
+            if (items[i] != null && items[i].getName().equals(key)) {
+                indexes[count] = i;
+                count++;
+            }
+        }
+        //присвоим результаты совпадений
+        Item[] result = new Item[count];
+        for (int index = 0; index != count; index++) {
+            result[index] = items[indexes[index]];
+        }
+        return result;
+    }
+
+    public Item findById(String id) {
+        Item result = null;
+        for (Item item: this.items) {
+            if (item != null && item.getId().equals(id)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
+    }
+    // вспомогательная функция, ищет индекс по id
+    public int findByIdIndex(String id) {
+        int result = -1;
+        for (int i = 0; i != this.position; i++) {
+            if (items[i] != null && items[i].getId().equals(id)) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+}
