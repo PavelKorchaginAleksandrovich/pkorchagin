2d2679be7d500b3464d64313467c54a3d325d942
PavelKorchaginAleksandrovich <pav.intense@gmail.com>
2018-01-11 23:53:04 +0300

    4. Используя шаблон проектирования - стратегию

--- chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
@@ -1,13 +1,45 @@
 package ru.job4j.strategy;
 
 import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
 import ru.job4j.models.Item;
 
 import static org.hamcrest.core.Is.is;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertArrayEquals;
 
 public class PaintTest {
+    @Test
+    public void whenDrawSquare() {
+        String squareString = new StringBuilder().append("+++++\n").
+                                                  append("+   +\n").
+                                                  append("+   +\n").
+                                                  append("+++++").
+                                                  append(System.lineSeparator()).toString();
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        new Paint().draw(new Square());
+        assertThat(new String(out.toByteArray()), is(squareString));
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        String squareString = new StringBuilder().append("   +   \n").
+                                                  append("  +++  \n").
+                                                  append(" +++++ \n").
+                                                  append("+++++++").
+                                                  append(System.lineSeparator()).toString();
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        new Paint().draw(new Triangle());
+        assertThat(new String(out.toByteArray()), is(squareString));
+    }
+
     @Test
     public void whenTringleThenPicTriangle() {
         String trangleString = new StringBuilder().append("   +   \n").
