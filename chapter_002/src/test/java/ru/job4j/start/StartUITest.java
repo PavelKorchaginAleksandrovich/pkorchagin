054e0c46afcd039326bed0f3dcf655c46d0f3ce5
PavelKorchaginAleksandrovich <pav.intense@gmail.com>
2018-01-17 19:52:49 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.

---
+++ chapter_002/src/test/java/ru/job4j/start/StartUITest.java
@@ -0,0 +1,105 @@
+package ru.job4j.start;
+import org.junit.Test;
+import ru.job4j.models.Item;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertArrayEquals;
+
+public class StartUITest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final String menu = "0. Add new Item\n"
+            + "1. Show all items\n"
+            + "2. Edit item\n"
+            + "3. Delete item\n"
+            + "4. Find item by Id\n"
+            + "5. Find items by name\n"
+            + "6. Exit Program\n";
+       public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
+
+    @Test
+    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+        Tracker tracker = new Tracker();
+        Input input = new StubInput(new String[]{"0", "new item", "desc", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.getByIndex(0).getName(), is("new item"));
+    }
+    @Test
+    public void whenEditThenTrackerHasEditValue() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("name", "decs"));
+        Input input = new StubInput(new String[]{"2", item.getId(), "edit name", "desc", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findById(item.getId()).getName(), is("edit name"));
+    }
+
+    @Test
+    public void whenDeleteItemThenTrackerNotHasItem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("name", "decs"));
+        Input input = new StubInput(new String[]{"3", item.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findAll().length, is(0));
+    }
+
+    @Test
+    public void whenFindItemThenItemByID() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "decs"));
+        String expected = new StringBuilder().
+                append(this.menu).append(System.lineSeparator()).
+                append(item.getName()).append(System.lineSeparator()).
+                append(this.menu).append(System.lineSeparator()).toString();
+        Input input = new StubInput(new String[]{"4", item.getId(), "6"});
+        StartUI start = new StartUI(input, tracker);
+        this.loadOutput();
+        start.init();
+        assertThat(new String(this.out.toByteArray()), is(expected));
+        this.backOutput();
+    }
+    @Test
+    public void whenFindItemNameThenItemByName() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "decs"));
+        String expected = new StringBuilder().append(this.menu).
+                append(System.lineSeparator()).
+                append("Найдено заявок 1").append(System.lineSeparator()).
+                append("Имя: ").append(item.getName()).append(" ID: ").append(item.getId()).
+                append(System.lineSeparator()).
+                append(this.menu).append(System.lineSeparator()).toString();
+        Input input = new StubInput(new String[]{"5", "test name", "6"});
+        StartUI start = new StartUI(input, tracker);
+        this.loadOutput();
+        start.init();
+        assertThat(new String(this.out.toByteArray()), is(expected));
+        this.backOutput();
+    }
+    @Test
+    public void whenShowAllThenAllItems() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "decs"));
+        String expected = new StringBuilder()
+                .append(this.menu).append(System.lineSeparator()).
+                append("Список всех заявок:").append(System.lineSeparator()).
+                append("Имя: ").append("test name").append(" ID: ").append(item.getId()).append(System.lineSeparator()).
+                append(this.menu).append(System.lineSeparator()).toString();
+        Input input = new StubInput(new String[]{"1", "6"});
+        StartUI start = new StartUI(input, tracker);
+        this.loadOutput();
+        start.init();
+        assertThat(new String(this.out.toByteArray()), is(expected));
+        this.backOutput();
+    }
+}
+
+
