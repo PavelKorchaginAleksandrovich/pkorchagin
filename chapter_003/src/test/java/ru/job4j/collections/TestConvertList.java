ea6ccc3aff3ff4fa0ee88e9e6aa1de83da02a3c1
PavelKorchaginAleksandrovich <pav.intense@gmail.com>
2018-01-31 21:10:14 +0300

    Коллекции

--- chapter_003/src/test/java/ru/job4j/collections/TestConvertList.java
+++ chapter_003/src/test/java/ru/job4j/collections/TestConvertList.java
@@ -1,13 +1,13 @@
 package ru.job4j.collections;
 
-import org.junit.Test;
+//import org.junit.Test;
 //import java.io.ByteArrayOutputStream;
 //import java.io.PrintStream;
 //import static org.hamcrest.core.Is.is;
 //import static org.junit.Assert.assertArrayEquals;
 //import static org.junit.Assert.assertThat;
 import java.util.*;
-public class TestConvertList {
-
-@Test
-}
+//public class TestConvertList {
+//
+//@Test
+//}
