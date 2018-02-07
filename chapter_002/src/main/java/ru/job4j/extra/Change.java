b03c41264f27696f11d6c5c6b3061404f6515792
PavelKorchaginAleksandrovich <pav.intense@gmail.com>
2018-02-07 18:22:01 +0300

    Считаем сдачу

--- chapter_002/src/main/java/ru/job4j/extra/Change.java
+++ chapter_002/src/main/java/ru/job4j/extra/Change.java
@@ -4,25 +4,21 @@ package ru.job4j.extra;
 import java.util.Arrays;
 
 public class Change {
+    private final static int[] CHANGE = {1, 2, 5, 10};
 
-    int[] change = {1, 2, 5, 10};
-    int index = 3;
-    int[] result;
-    int count = 0;
     public int[] changes(int value, int price) {
+        int count = 0;
+        int index = CHANGE.length - 1;
         int total = value - price;
-        this.result = new int[total];
+        int[] result = new int[total];
         while (total != 0) {
-           int numbers = total / change[index];
-            total -= change[index] * numbers;
-            addChanges(index--, numbers);
+           int numbers = total / CHANGE[index];
+            total -= CHANGE[index] * numbers;
+            for (int i = 0; i != numbers; i++) {
+                result[count++] = CHANGE[index];
+            }
+            index--;
         }
-
         return Arrays.copyOf(result, count);
-    }
-    public void  addChanges(int index, int numbers) {
-        for (int i = 0; i != numbers; i++) {
-            this.result[this.count++] = change[index];
-        }
     }
 }
