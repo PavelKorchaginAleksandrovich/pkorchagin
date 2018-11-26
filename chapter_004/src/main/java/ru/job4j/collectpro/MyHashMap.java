0681a036342ae01065ffd20ec6f72c012a47d3f3
PavelKorchaginAleksandrovich <pav.intense@gmail.com>
2018-11-26 10:59:27 +0300

    8. Реализовать собственную структуру данных - HashMap

---
+++ chapter_004/src/main/java/ru/job4j/collectpro/MyHashMap.java
@@ -0,0 +1,112 @@
+package ru.job4j.collectpro;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MyHashMap<K, V> implements Iterable {
+
+    private Object[] container;
+    private int size = 10;
+    private int count = 0;
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public MyHashMap() {
+        container = new Object[size];
+    }
+
+    boolean insert(K key, V value) {
+        boolean result = false;
+        int position = key.hashCode() % size;
+        if (container[position] == null) {
+            Element element = new Element(key, value);
+            container[position] = element;
+            result = true;
+            count++;
+        }
+        // увеличим размер массива когда он будет заполнен наполовину
+        if (count > size / 2) {
+            increase();
+        }
+        return result;
+    }
+
+    V get(K key) {
+        Element elem = (Element) container[key.hashCode() % size];
+
+        return elem == null ? null : (elem.getKey().equals(key) ? elem.getValue() : null);
+    }
+
+    boolean delete(K key) {
+        boolean result = false;
+        int pos = key.hashCode() % size;
+        Element elem = (Element) container[pos];
+        if (elem != null && elem.getKey().equals(key)) {
+            container[pos] = null;
+            result = true;
+        }
+        return result;
+    }
+
+
+    void increase() {
+        size *= 2;
+        Object[] newContainer = new Object[size];
+        for (Object elem : container) {
+            if (elem != null) {
+                Element el = (Element) elem;
+                K key = el.getKey();
+                newContainer[key.hashCode() % size] = elem;
+            }
+        }
+        container = newContainer;
+    }
+
+
+    //класс для хранения данных,
+    private class Element {
+        private K key;
+        private V value;
+
+        Element(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+
+    public Iterator iterator() {
+        return new Itr();
+    }
+
+    private class Itr implements Iterator<V> {
+        private int cursor = 0;
+
+
+        @Override
+        public boolean hasNext() {
+            return cursor != size;
+        }
+
+        @Override
+        public V next() {
+            if (cursor >= container.length) {
+                throw new NoSuchElementException();
+            }
+            Element elem = (Element) container[cursor];
+            cursor += 1;
+            return elem.getValue();
+        }
+    }
+
+}
---
+++ chapter_004/src/test/java/ru/job4j/collectpro/MyHashMapTest.java
@@ -0,0 +1,104 @@
+package ru.job4j.collectpro;
+
+import static org.hamcrest.Matchers.is;
+
+import org.hamcrest.core.Is;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
+
+public class MyHashMapTest {
+
+    @Test
+    public void insert() {
+        MyHashMap<Integer, String> myHashMap = new MyHashMap<>();
+        assertThat(myHashMap.insert(10, "Test"), Is.is(true));
+        assertThat(myHashMap.insert(20, "Test2"), Is.is(false));
+        assertThat(myHashMap.insert(5, "Test3"), Is.is(true));
+    }
+
+    @Test
+    public void get() {
+        MyHashMap<Integer, String> myHashMap = new MyHashMap<>();
+        myHashMap.insert(10, "Test");
+        myHashMap.insert(5, "Test3");
+        assertThat(myHashMap.get(20), Is.is((String) null));
+        assertThat(myHashMap.get(10), Is.is("Test"));
+        assertThat(myHashMap.get(5), Is.is("Test3"));
+
+    }
+
+    @Test
+    public void delete() {
+        MyHashMap<Integer, String> myHashMap = new MyHashMap<>();
+        myHashMap.insert(10, "Test");
+        myHashMap.insert(5, "Test3");
+        myHashMap.insert(3, "Test4");
+        assertThat(myHashMap.delete(20), Is.is(false));
+        assertThat(myHashMap.get(5), Is.is("Test3"));
+        assertThat(myHashMap.delete(5), Is.is(true));
+        assertThat(myHashMap.delete(5), Is.is(false));
+        assertThat(myHashMap.get(5), Is.is((String) null));
+
+    }
+
+    @Test
+    public void increase() {
+        MyHashMap<Integer, String> myHashMap = new MyHashMap<>();
+        assertThat(myHashMap.getSize(), Is.is(10));
+        assertThat(myHashMap.insert(9, "Test9"), Is.is(true));
+        assertThat(myHashMap.insert(19, "Test19"), Is.is(false));
+        assertThat(myHashMap.insert(1, "Test1"), Is.is(true));
+        assertThat(myHashMap.insert(2, "Test2"), Is.is(true));
+        assertThat(myHashMap.insert(3, "Test3"), Is.is(true));
+        assertThat(myHashMap.insert(4, "Test4"), Is.is(true));
+        assertThat(myHashMap.insert(5, "Test5"), Is.is(true));
+        assertThat(myHashMap.insert(6, "Test6"), Is.is(true));
+        assertThat(myHashMap.insert(19, "Test19"), Is.is(true));
+        assertThat(myHashMap.getSize(), Is.is(20));
+        assertThat(myHashMap.get(1), Is.is("Test1"));
+        assertThat(myHashMap.get(2), Is.is("Test2"));
+        assertThat(myHashMap.get(3), Is.is("Test3"));
+        assertThat(myHashMap.get(4), Is.is("Test4"));
+        assertThat(myHashMap.get(5), Is.is("Test5"));
+        assertThat(myHashMap.get(6), Is.is("Test6"));
+        assertThat(myHashMap.get(9), Is.is("Test9"));
+        assertThat(myHashMap.get(19), Is.is("Test19"));
+    }
+
+    @Test()
+    public void iterator() {
+        MyHashMap<Integer, String> myHashMap = new MyHashMap<>();
+        assertThat(myHashMap.insert(1, "Test1"), Is.is(true));
+        assertThat(myHashMap.insert(2, "Test2"), Is.is(true));
+        assertThat(myHashMap.insert(3, "Test3"), Is.is(true));
+        assertThat(myHashMap.insert(4, "Test4"), Is.is(true));
+        Iterator itr = myHashMap.iterator();
+        assertThat(itr.hasNext(), Is.is(true));
+        assertThat(itr.next(), Is.is((String) null));
+        assertThat(itr.hasNext(), Is.is(true));
+        assertThat(itr.next(), Is.is("Test1"));
+        assertThat(itr.hasNext(), Is.is(true));
+        assertThat(itr.next(), Is.is("Test2"));
+        assertThat(itr.hasNext(), Is.is(true));
+        assertThat(itr.next(), Is.is("Test3"));
+        assertThat(itr.hasNext(), Is.is(true));
+        assertThat(itr.next(), Is.is("Test4"));
+        assertThat(itr.hasNext(), Is.is(true));
+        assertThat(itr.next(), Is.is((String) null));
+        assertThat(itr.hasNext(), Is.is(true));
+        assertThat(itr.next(), Is.is((String) null));
+        assertThat(itr.hasNext(), Is.is(true));
+        assertThat(itr.next(), Is.is((String) null));
+        assertThat(itr.hasNext(), Is.is(true));
+        assertThat(itr.next(), Is.is((String) null));
+        assertThat(itr.hasNext(), Is.is(true));
+        assertThat(itr.next(), Is.is((String) null));
+        assertThat(itr.hasNext(), Is.is(false));
+//        itr.next();
+    }
+}
